871b85a4ab168b24b343ed3172efc160ad82ce60
tanay0209 <tanayjagnani@gmail.com>
2024-10-13 18:00:57 +0530

    handled bug

--- client/src/redux/slices/apiSlice.ts
+++ client/src/redux/slices/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_URL,
+        baseUrl: "https://code-block-anfg.onrender.com",
         credentials: "include"
     }),
     tagTypes: ["user-codes", "all-codes"],
